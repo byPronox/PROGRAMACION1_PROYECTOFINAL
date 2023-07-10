7fdb4c320f1357b971db973eb8a843edde61b10e
byPronox <bypronoxyt@gmail.com>
2023-07-09 21:32:31 -0500

    Inicio creacion de librerias a utilizar y struct para los datos deseados PROPUESTA 2

---
+++ PROPUESTA 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+ 
+
+struct OrdenTrabajo {
+    int numeroOrden;
+    char tipoEquipo[50];
+    char servicio[50];
+    float costo;
+    char fechaCreacion[20];
+};
+
