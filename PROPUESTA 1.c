119dcc5bcfdfdbc7c412c85e781b10c1f84f53be
byPronox <bypronoxyt@gmail.com>
2023-07-09 20:27:19 -0500

    Abrimos archivo .txt para imprimir datos

--- PROPUESTA 1.c
+++ PROPUESTA 1.c
@@ -11,3 +11,20 @@ struct OrdenTrabajo {
     char tipoTrabajo[50];
     float costoServicio;
 };
+
+struct OrdenTrabajo ordenes[MAX_ORDERS];
+int numOrdenes = 0;
+
+void guardarOrdenes() {
+    FILE *archivo = fopen(FILENAME, "w");
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo.\n");
+        return;
+    }
+
+    for (int i = 0; i < numOrdenes; i++) {
+        fprintf(archivo, "%d %s %s %.2f\n", ordenes[i].numero, ordenes[i].fecha, ordenes[i].tipoTrabajo, ordenes[i].costoServicio);
+    }
+
+    fclose(archivo);
+}
